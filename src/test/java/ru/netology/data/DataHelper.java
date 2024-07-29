a7fc18f2c09cd867b228183b7ec4b17953337884
zdorovilov test <zdorovilovde@mail.ru>
2024-07-29 22:35:50 +0500

    Подготовил данные в DataHelper

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,41 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    public static VerificationCode getVerificationCode() {
+        return new VerificationCode("123456");
+    }
+
+    @Value
+    public static class CardInfo {
+        String number;
+        String testId;
+    }
+
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static CardInfo getSecondCardInfo() {
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+}
