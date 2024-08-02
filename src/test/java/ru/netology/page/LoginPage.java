3e50168c60fb0fc1e3153d014d92fcd665fcab21
zdorovilov test <zdorovilovde@mail.ru>
2024-08-02 23:53:05 +0500

    Update LoginPage

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -5,7 +5,6 @@ import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
-
 public class LoginPage {
     private SelenideElement loginField = $("[data-test-id=login] input");
     private SelenideElement passwordField = $("[data-test-id=password] input");
